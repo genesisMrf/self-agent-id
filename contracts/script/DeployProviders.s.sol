f87f73bfdba6e63dc9c5befcab2a0c8c0a2fd9d1
Tranquil-Flow <tranquil_flow@protonmail.com>
2026-02-26 02:22:15 +1000

    deploy: all contracts to Celo Mainnet and Sepolia (v1)
      Deploy all contracts to Celo Mainnet (42220) and Celo Sepolia (11142220).
    
      Mainnet contracts:
        SelfAgentRegistry (proxy): 0xaC3DF9ABf80d0F5c020C06B04Cced27763355944
        SelfHumanProofProvider:    0x4b036aFD959B457A208F676cf44Ea3ef73Ea3E3d
        SelfReputationRegistry:    0x69Da18CF4Ac27121FD99cEB06e38c3DC78F363f4
        SelfValidationRegistry:    0x71a025e0e338EAbcB45154F8b8CA50b41e7A0577
        AgentDemoVerifier:          0xD8ec054FD869A762bC977AC328385142303c7def
        AgentGate:                  0x26e05bF632fb5bACB665ab014240EAC1413dAE35
    
      - Document weights configured (passport=100, id-card=100, aadhaar=80, kyc=50)
      - Multisig roles granted (deployer not yet renounced)
      - Removed legacy SelfReputationSignal/SelfFreshnessChecker from DeployProviders
      - Updated registry.json with all deployment addresses

--- contracts/script/DeployProviders.s.sol
+++ contracts/script/DeployProviders.s.sol
@@ -4,18 +4,15 @@ pragma solidity 0.8.28;
 
 import { console } from "forge-std/console.sol";
 import { BaseScript } from "./Base.s.sol";
-import { SelfReputationSignal } from "../src/SelfReputationSignal.sol";
-import { SelfFreshnessChecker } from "../src/SelfFreshnessChecker.sol";
 import { SelfReputationRegistry } from "../src/SelfReputationRegistry.sol";
 import { SelfValidationRegistry } from "../src/SelfValidationRegistry.sol";
 import { ProxyRoot } from "../src/upgradeable/ProxyRoot.sol";
 
 /// @title DeployProviders
-/// @notice Deploy SelfReputationRegistry (UUPS proxy) + SelfValidationRegistry (UUPS proxy) +
-///         SelfReputationSignal + SelfFreshnessChecker
+/// @notice Deploy SelfReputationRegistry (UUPS proxy) + SelfValidationRegistry (UUPS proxy)
 /// @dev Usage:
 ///   PRIVATE_KEY=0x... REGISTRY=0x... forge script script/DeployProviders.s.sol \
-///     --rpc-url celo-sepolia --broadcast --verify --evm-version cancun
+///     --rpc-url celo-mainnet --broadcast --verify --evm-version cancun --code-size-limit 50000
 contract DeployProviders is BaseScript {
     function run() external broadcast {
         address registry = vm.envAddress("REGISTRY");
@@ -39,12 +36,5 @@ contract DeployProviders is BaseScript {
         )));
         console.log("SelfValidationRegistry impl:", address(valImpl));
         console.log("SelfValidationRegistry proxy:", address(val));
-
-        // Deploy non-upgradeable signal/checker contracts
-        SelfReputationSignal repSignal = new SelfReputationSignal(registry);
-        SelfFreshnessChecker freshnessChecker = new SelfFreshnessChecker(registry);
-
-        console.log("SelfReputationSignal:", address(repSignal));
-        console.log("SelfFreshnessChecker:", address(freshnessChecker));
     }
 }
